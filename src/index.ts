04e2acd41e5050baccb0e64bf2df7b95779a9ec8
GiulianoDami <38913937+GiulianoDami@users.noreply.github.com>
2026-06-06 08:03:23 +0200

    Code fix

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './beluga-mating-analyzer';
+export * from './genetic-diversity-calculator';
+export * from './mating-pattern-analyzer';
+export * from './paternity-analyzer';
+export * from './population-health-indicator';
